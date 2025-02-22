9e1a0f2951d19075910086e0519c0a79e4bf737f
radwaadel17 <radwaadel959@gmail.com>
2025-02-23 00:29:40 +0200

    add onboarding to approuter

--- app/lib/core/utlis/app_router.dart
+++ app/lib/core/utlis/app_router.dart
@@ -1,12 +1,18 @@
+import 'package:app/features/on_boarding_screen/presentation/views/on_boarding_screen.dart';
 import 'package:app/features/spalsh_screen/presentation/views/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class Approuter {
+  static const onBoardingKey = '/onBoardingScreen';
   static final router = GoRouter(
     routes: [
       GoRoute(
         path: "/",
         builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: onBoardingKey,
+        builder: (context, state) => const OnBoardingScreen(),
       ),
     ],
   );
