e79cc8c80981971dcbf34e2e8bc487873dd6563b
radwaadel17 <radwaadel959@gmail.com>
2025-02-22 20:00:06 +0200

    add app_images class for logo images

---
+++ app/lib/core/utlis/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const photoLogo1 = 'assets/images/1.png';
+  static const photoLogo2 = 'assets/images/2.png';
+  static const photoLogo3 = 'assets/images/3.png';
+  static const photoLogo4 = 'assets/images/4.png';
+}
--- app/lib/features/spalsh_screen/presentation/views/widgets/splash_screen_body.dart
+++ app/lib/features/spalsh_screen/presentation/views/widgets/splash_screen_body.dart
@@ -5,8 +5,6 @@ class SpalshScreenBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
-    );
+    return const Scaffold();
   }
 }
