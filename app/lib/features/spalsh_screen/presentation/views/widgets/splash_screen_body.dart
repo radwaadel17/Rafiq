1d5a8fd1e18ce3acdc27d528e15e995fa08fa979
radwaadel17 <radwaadel959@gmail.com>
2025-02-22 20:54:37 +0200

    edit logo photos for animation

--- app/lib/features/spalsh_screen/presentation/views/widgets/splash_screen_body.dart
+++ app/lib/features/spalsh_screen/presentation/views/widgets/splash_screen_body.dart
@@ -1,7 +1,6 @@
 import 'package:app/core/utlis/app_images.dart';
 import 'package:app/core/utlis/device_size.dart';
 import 'package:app/core/utlis/text_style.dart';
-import 'package:app/features/spalsh_screen/presentation/views/widgets/row_logo_photos.dart';
 import 'package:app/features/spalsh_screen/presentation/views/widgets/text_arabic_with_style.dart';
 import 'package:flutter/material.dart';
 
@@ -14,16 +13,54 @@ class SpalshScreenBody extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const RowLogoPhotos(relativePath1: AppImages.photoLogo1 , relativePath2: AppImages.photoLogo2,),
-          const SizedBox(height: 10,),
-          const RowLogoPhotos(relativePath1: AppImages.photoLogo3 , relativePath2: AppImages.photoLogo4,),
-          const SizedBox(height: 15,),
-          TextArabicWithStyle(text: 'أهلاً، أنا رَفيق', textsyle: Styles.textstyle24.copyWith(fontWeight: FontWeight.w600),),
-          const  SizedBox(height: 10,),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 48,
+                  child: Image(image: AssetImage(AppImages.photoLogo1))),
+              SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                  height: 48,
+                  child: Image(image: AssetImage(AppImages.photoLogo2))),
+            ],
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 48,
+                  child: Image(image: AssetImage(AppImages.photoLogo3))),
+              SizedBox(
+                width: 10,
+              ),
+              SizedBox(
+                  height: 48,
+                  child: Image(image: AssetImage(AppImages.photoLogo4))),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          TextArabicWithStyle(
+            text: 'أهلاً، أنا رَفيق',
+            textsyle: Styles.textstyle24.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
           SizedBox(
             width: context.screenWidth * 0.9,
-            child: const TextArabicWithStyle(text: 'مساعدك الأمثل لدعم الأطفال ذوي التوحد، يقربك من الأطباء، يوفر لك مجتمعًا للآباء، ويقدم حلولًا مبتكرة لتعليم طفلك', textsyle: Styles.textstyle24,
-            textAlign: TextAlign.center,
+            child: const TextArabicWithStyle(
+              text:
+                  'مساعدك الأمثل لدعم الأطفال ذوي التوحد، يقربك من الأطباء، يوفر لك مجتمعًا للآباء، ويقدم حلولًا مبتكرة لتعليم طفلك',
+              textsyle: Styles.textstyle24,
+              textAlign: TextAlign.center,
             ),
           ),
         ],
@@ -31,6 +68,3 @@ class SpalshScreenBody extends StatelessWidget {
     );
   }
 }
-
-
-
