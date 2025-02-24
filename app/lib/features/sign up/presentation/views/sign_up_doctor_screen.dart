f08c3bc1e213f5d523263942cfd83a7ef27bb461
radwaadel17 <radwaadel959@gmail.com>
2025-02-24 19:51:33 +0200

    create file of sign up doctor

---
+++ app/lib/features/sign up/presentation/views/sign_up_doctor_screen.dart
@@ -0,0 +1,11 @@
+import 'package:app/features/sign%20up/presentation/views/widgets/sign_up_doctor_screen_body.dart';
+import 'package:flutter/material.dart';
+
+class SignUpDoctorScreen extends StatelessWidget {
+  const SignUpDoctorScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SignUpDoctorBody();
+  }
+}
