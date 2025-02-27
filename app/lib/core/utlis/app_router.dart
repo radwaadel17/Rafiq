f3a512bebb28a56429d75d2e63255dcb12fc7059
radwaadel17 <radwaadel959@gmail.com>
2025-02-27 15:06:29 +0200

    add fade transition to app router

--- app/lib/core/utlis/app_router.dart
+++ app/lib/core/utlis/app_router.dart
@@ -3,6 +3,7 @@ import 'package:app/features/sign%20in/presentation/views/sign_in_screen.dart';
 import 'package:app/features/sign%20up/presentation/views/sign_up_doctor_screen.dart';
 import 'package:app/features/sign%20up/presentation/views/sign_up_parents_screen.dart';
 import 'package:app/features/spalsh_screen/presentation/views/splash_screen.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class Approuter {
@@ -11,8 +12,6 @@ abstract class Approuter {
   static const signUpParentsScreenKey = '/SignUpParents';
   static const signIn = '/SignIn';
 
-
-
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -20,20 +19,56 @@ abstract class Approuter {
         builder: (context, state) => const SplashScreen(),
       ),
       GoRoute(
-        path: onBoardingKey,
-        builder: (context, state) => const OnBoardingScreen(),
+        path: signUpDoctorScreenKey,
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: const SignUpDoctorScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
       ),
       GoRoute(
-        path: signUpDoctorScreenKey,
-        builder: (context, state) => const SignUpDoctorScreen(),
-      ),
-       GoRoute(
         path: signUpParentsScreenKey,
-        builder: (context, state) => const SignUpParentsScreen(),
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: const SignUpParentsScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
       ),
-       GoRoute(
+      GoRoute(
         path: signIn,
-        builder: (context, state) => const SignInScreen(),
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: const SignInScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
+      ),
+      GoRoute(
+        path: onBoardingKey,
+        pageBuilder: (context, state) => CustomTransitionPage(
+          key: state.pageKey,
+          child: const OnBoardingScreen(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          },
+        ),
       ),
     ],
   );
