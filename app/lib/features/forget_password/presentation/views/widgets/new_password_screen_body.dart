d499daa66c0b4d0564873a5d483d05f3306671b2
radwaadel17 <radwaadel959@gmail.com>
2025-03-02 16:03:08 +0200

    add files of new password body screen

---
+++ app/lib/features/forget_password/presentation/views/widgets/new_password_screen_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewPasswordBody extends StatelessWidget {
+  const NewPasswordBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
