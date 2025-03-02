1f51e934b39f111cb2a9d42b93cc98cb3d07921e
radwaadel17 <radwaadel959@gmail.com>
2025-03-02 14:55:13 +0200

    add forget password screen files

---
+++ app/lib/features/forget_password/presentation/views/forget_password_screen.dart
@@ -0,0 +1,11 @@
+import 'package:app/features/forget_password/presentation/views/widgets/forget_password_body.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ForgetPasswordBody();
+  }
+}
