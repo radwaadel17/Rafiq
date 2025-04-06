2b42739cea4759f00a34316a04f504a2519f2f44
radwaadel17 <radwaadel959@gmail.com>
2025-04-06 19:53:21 +0200

    implemnt state of forget password cubit

---
+++ app/lib/features/forget_password/presentation/manager/forget password cubit/forget_password_states.dart
@@ -0,0 +1,15 @@
+class ForgetPasswordState {}
+
+class ForgetPasswordStateinit extends ForgetPasswordState {}
+
+class ForgetPasswordFaluireState extends ForgetPasswordState {
+  final String errorMessage;
+  ForgetPasswordFaluireState(this.errorMessage);
+}
+
+class ForgetPasswordLoadingState extends ForgetPasswordState {}
+
+class ForgetPasswordSucssesState extends ForgetPasswordState {
+  final String msg;
+  ForgetPasswordSucssesState(this.msg);
+}
