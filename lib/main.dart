fe2fce73f0c0ff2ceb5e60f4aed7a022fab078fe
radwaadel17 <radwaadel959@gmail.com>
2025-04-23 22:41:19 +0200

    initialize localization in app

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:app/features/sign%20in/data/repos/sign_in_repo_imp.dart';
 import 'package:app/features/sign%20in/presentation/manager/sign_in_cubit/sign_in_cubit.dart';
 import 'package:app/features/sign%20up/data/repos/sign_up_repo_imp.dart';
 import 'package:app/features/sign%20up/presentation/manager/signup%20cubit/sign_up_cubit.dart';
+import 'package:app/generated/l10n.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 
 void main() {
   setup();
@@ -27,6 +29,7 @@ class RafeekApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
@@ -36,19 +39,19 @@ class RafeekApp extends StatelessWidget {
           BlocProvider(create: (context) => SignInCubit(getIt<SignInRepoImp>())),
         ],
         child: MaterialApp.router(
+         // locale : Locale('ar'),
+           localizationsDelegates: const [
+                S.delegate,
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
           locale: DevicePreview.locale(context),
           builder: DevicePreview.appBuilder,
           routerConfig: Approuter.router,
           debugShowCheckedModeBanner: false,
-          supportedLocales: const [
-            Locale('en'),
-            Locale('ar'),
-          ],
-          localizationsDelegates: const [
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
+          
           theme: ThemeData(
             fontFamily: 'Alexandria',
             scaffoldBackgroundColor: Colors.white,
