a7d5307d6e2a7d4a71c2998e2b4c321e46fb08e4
radwaadel17 <radwaadel959@gmail.com>
2025-04-27 19:32:48 +0300

    add profile and profile view body files

---
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:app/features/profile/presentation/views/widgets/profile_view_body.dart';
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: ProfileViewBody(),
+    );
+  }
+}
