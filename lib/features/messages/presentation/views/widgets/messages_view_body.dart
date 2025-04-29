dde3836e47ea12a92ed1dc8963667972c881bf5b
radwaadel17 <radwaadel959@gmail.com>
2025-04-29 20:17:31 +0300

    create messages feature and add files

---
+++ lib/features/messages/presentation/views/widgets/messages_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MessageViewBody extends StatelessWidget {
+  const MessageViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+
+      ],
+    );
+  }
+}
+
