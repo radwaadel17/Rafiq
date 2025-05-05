b9015907cf5c6775133ba227dfddd63beca4ae62
radwaadel17 <radwaadel959@gmail.com>
2025-05-05 21:29:14 +0300

    add google genrative ai package and refactor code

--- lib/features/chat bot/presentation/views/widgets/chat_bot.dart
+++ lib/features/chat bot/presentation/views/widgets/chat_bot.dart
@@ -1,8 +1,8 @@
 import 'package:app/core/utlis/constants.dart';
 import 'package:app/core/utlis/device_size.dart';
-import 'package:app/core/utlis/text_style.dart';
 import 'package:app/features/chat%20bot/presentation/views/text_field_chat_bot.dart';
 import 'package:app/features/chat%20bot/presentation/views/widgets/chat_bot_bubble.dart';
+import 'package:app/features/chat%20bot/presentation/views/widgets/user_chat_bot_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -38,26 +38,3 @@ class ChatBot extends StatelessWidget {
   }
 }
 
-class UserMessageChatBotBubble extends StatelessWidget {
-  const UserMessageChatBotBubble({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.topLeft,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
-        child: Flexible(
-            child: Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-              color: colorApp, borderRadius: BorderRadius.circular(5.r)),
-          child: Text(
-            'طفلي يعاني من  ..',
-            style: Styles.textstyle14.copyWith(color: Colors.white),
-          ),
-        )),
-      ),
-    );
-  }
-}
---
+++ lib/features/chat bot/presentation/views/widgets/user_chat_bot_bubble.dart
@@ -0,0 +1,28 @@
+import 'package:app/core/utlis/constants.dart';
+import 'package:app/core/utlis/text_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class UserMessageChatBotBubble extends StatelessWidget {
+  const UserMessageChatBotBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topLeft,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 10.h),
+        child: Flexible(
+            child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              color: colorApp, borderRadius: BorderRadius.circular(5.r)),
+          child: Text(
+            'طفلي يعاني من  ..',
+            style: Styles.textstyle14.copyWith(color: Colors.white),
+          ),
+        )),
+      ),
+    );
+  }
+}
