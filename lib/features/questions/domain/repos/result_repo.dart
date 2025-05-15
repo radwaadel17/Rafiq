e564f4b8f7db892e057fe5216614c5c199fb4f26
radwaadel17 <radwaadel959@gmail.com>
2025-05-15 22:35:05 +0300

    Add ResultRepo interface for fetching results; define getResult method with QsEntity parameter.

---
+++ lib/features/questions/domain/repos/result_repo.dart
@@ -0,0 +1,8 @@
+import 'package:app/core/errors/faluire_model.dart';
+import 'package:app/features/questions/domain/entity/qs_entity.dart';
+import 'package:app/features/questions/domain/entity/result_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class ResultRepo {
+  Future<Either<Faluire , ResultEntity>>getResult({required QsEntity qsEntity});
+}
